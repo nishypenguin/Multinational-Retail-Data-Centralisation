654998becced42eadce0f8746f2ebd7a4b0a668d
nishypenguin <anoshmalik@Anoshs-MacBook-Pro.local>
2024-08-19 10:38:58 +0100

    New commit

--- database_utils.py
+++ database_utils.py
@@ -1,1 +1,14 @@
+#import psycopg2
+import yaml 
+
 class DataBaseConnector:
+
+    def __init__(self, creds_file='db_creds.yaml'):
+        self.creds_file = creds_file
+
+    def read_db_creds(self):
+       with open (self.creds_file, 'r') as file:
+           creds = yaml.safe_load(file)
+           return creds 
+
+

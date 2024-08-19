e5d657bce608f56696ad5df72ebf38ba83f11a14
nishypenguin <anoshmalik@Anoshs-MacBook-Pro.local>
2024-08-19 10:39:40 +0100

    New commit 2

--- database_utils.py
+++ database_utils.py
@@ -1,4 +1,4 @@
-#import psycopg2
+import psycopg2
 import yaml 
 
 class DataBaseConnector:
@@ -10,5 +10,25 @@ class DataBaseConnector:
        with open (self.creds_file, 'r') as file:
            creds = yaml.safe_load(file)
            return creds 
+
+    def init_db_engine(self,creds):
+        HOST = creds['RDS_HPST']
+        USER = creds['RDS_USER']
+        PASSWORD = creds['RDS_PASSWORD']
+        DATABASE = creds['RDS_DATABASE']
+        PORT = creds['RDS_PORT']
+
+        with psycopg2.connect(host=HOST, user=USER, password=PASSWORD, dbname=DATABASE, port=PORT) as conn:
+            with conn.cursor() as cur:
+                cur.execute('''CREATE TABLE actor_2 AS (
+                            SELECT * FROM actor
+                            LIMIT 10);
+
+                            SELECT * FROM actor_2''')
+                print(type(cur))
+                records = cur.fetchall()
+
+
+
 
 
